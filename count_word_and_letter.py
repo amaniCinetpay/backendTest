444542c28366b8725ed0f3c258764b54b6f0a31f
amaniCinetpay <amani.kouakou@cinetpay.com>
2021-07-16 18:44:18 +0000

    complexité

--- count_word_and_letter.py
+++ count_word_and_letter.py
@@ -1,4 +1,4 @@
-
+import unittest
 #-----------------------Problème 1---------------------------------------
 # La fonction pour avoir le nombre d'occurence des mots de Alice
 def count_word(book,word):
@@ -15,6 +15,17 @@ def count_letter(word) :
     return str(a).replace(": ","").replace("{","[").replace("}","]")
 #------------------------------------------------------------------------
 
+
+
+
+class TestCount_word(unittest.TestCase):
+
+    def test_valid(self):
+         self.assertEqual( count_word( ["1","2","3","85","1","1","5","88","100","1","78","789","777","0","-1","4485","4100","741"], "3"),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 #-----------------------Problème 3---------------------------------------
 # Etant donné que la bloucle "FOR" est appliquée une seule fois  dans les problèmes 1&2 sur un tableau 
